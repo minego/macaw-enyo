dedf6cb6cca692f7ed017fa1ab647c9b7fb53336
Micah N Gorrell <github@minego.net>
2013-01-09 00:57:35 -0700

    Minor tweaks

--- source/main.js
+++ source/main.js
@@ -361,8 +361,9 @@ closeAllToasters: function(sender, event)
 
 refresh: function(sender, event)
 {
-	// TODO	write me
-	this.log('write me...');
+	for (var t = 0, tab; tab = this.tabs[t]; t++) {
+		this.$['panel' + t].refresh();
+	}
 },
 
 back: function(sender, event)
