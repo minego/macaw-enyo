157d21d4f63c4fc2058cbbbfc8bb447ad76b3958
Micah N Gorrell <github@minego.net>
2013-03-04 19:44:20 -0700

    Re-enabled caching. I seem to accidentially commit with caching disabled a lot. Sorry about that.

--- source/messages/list.js
+++ source/messages/list.js
@@ -133,7 +133,7 @@ rendered: function()
 	/* Load cached messages */
 	var results;
 
-	if (false && this.cache) {
+	if (this.cache) {
 		results = prefs.get('cachedmsgs:' + this.user.id + ':' + this.resource) || [];
 	} else {
 		results = [];
