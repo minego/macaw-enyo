8730731c36332e458c77484bea4a20d7dab1780a
Micah N Gorrell <m@minego.net>
2013-07-05 02:30:26 -0600

    Tweaking, and trying to improve oauth on phones

--- source/twitter.js
+++ source/twitter.js
@@ -176,9 +176,11 @@ authorize: function(cb, params, pin)
 			Step 1:	Request an authorization token, and open a browser
 					window so that the user may get a PIN.
 		*/
-		this.oauth.get(this.apibase + '/oauth/request_token',
+		this.oauth.post(this.apibase + '/oauth/request_token', { },
 			function(response) {
-				window.open('https://twitter.com/oauth/authorize?' + response.text, "_auth");
+				window.open('https://twitter.com/oauth/authorize?' +
+					response.text, '_auth');
+
 				cb(response.text);
 			},
 
