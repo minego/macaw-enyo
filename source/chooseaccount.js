277e042641736b0be78c06ff07dbcce55dfb3dc4
Micah N Gorrell <m@minego.net>
2013-07-14 23:49:49 -0600

    Added forgotten file used for cross posting

---
+++ source/chooseaccount.js
@@ -0,0 +1,215 @@
+/*
+	Copyright (c) 2010, Micah N Gorrell
+	All rights reserved.
+
+	THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+	WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+	MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+	EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+	PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+	OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+	WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+	OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+	ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+enyo.kind({
+
+name:								"ChooseAccount",
+
+/* Show all fields in the detail toaster */
+classes:							"chooseaccount confirm",
+
+published: {
+	title:							"Select one or more account?",
+	command:						null,
+
+	users:							[],
+	multi:							true,
+	buttons:						[]
+},
+
+handlers: {
+	ontap:							"handleCommand"
+},
+
+events: {
+	onCloseToaster:					"",
+	onChoose:						""
+},
+
+components: [
+	{
+		kind:						enyo.Signals,
+		onkeypress:					"keypress"
+	},
+	{
+		name:						"title",
+		classes:					"title"
+	},
+
+	{
+		name:						"users",
+		fit:						true,
+		classes:					"users"
+	},
+
+	{
+		classes:					"controls",
+		layoutKind:					"FittableColumnsLayout",
+
+		components: [
+			{
+				name:				"buttons",
+				fit:				true,
+				classes:			"center"
+			}
+		]
+	}
+],
+
+create: function()
+{
+	this.inherited(arguments);
+
+	this.$.title.setContent(this.title);
+
+	for (var i = 0, o; o = this.buttons[i]; i++) {
+		var button = this.$.buttons.createComponent({
+			kind:			onyx.Button,
+			classes:		o.classes || "button",
+			content:		o.content,
+			command:		"done",
+			usercommand:	o.command
+		}, { owner: this });
+	}
+
+	this.usersChanged();
+},
+
+keypress: function(sender, event)
+{
+	if (event && event.which == 13) {
+		/* Send default button on enter (regardless of modifier) */
+		for (var i = 0, o; o = this.buttons[i]; i++) {
+			if (o.onenter) {
+				this.handleCommand({
+					usercommand:	o.command
+				}, {
+					command:		"done"
+				});
+			}
+		}
+	}
+},
+
+usersChanged: function()
+{
+	var		i, u;
+
+	this.$.users.destroyClientControls();
+
+	/* Ensure that at least one is enabled */
+	for (i = 0; u = this.users[i]; i++) {
+		if (u.enabled) {
+			break;
+		}
+	}
+	if (!this.users[i]) {
+		this.users[0].enabled = true;
+	}
+
+
+	for (i = 0; u = this.users[i]; i++) {
+		var item = this.$.users.createComponent({
+			name:			'user' + i,
+			classes:		'user',
+			command:		this.multi ? 'toggle' : 'select',
+			userid:			u.id,
+
+			content:		'@' + u.screenname,
+			style:			'background: url(' + u.profile.avatar + ') left center no-repeat'
+		}, { owner: this });
+
+		if (u.enabled) {
+			item.addClass('enabled');
+		}
+	}
+	this.$.users.render();
+},
+
+handleCommand: function(sender, event)
+{
+	/* Find the real sender */
+	if (event.dispatchTarget) {
+		sender = event.dispatchTarget;
+	}
+
+	cmd = sender.command || event.command;
+
+	switch (cmd) {
+		case "select":
+			for (var i = 0, u; u = this.users[i]; i++) {
+				if (sender.userid == u.id) {
+					u.enabled = true;
+				} else {
+					u.enabled = false;
+				}
+			}
+			/* fallthrough */
+
+		case "done":
+			this.doCloseToaster();
+
+			this.doChoose({
+				command:		sender.usercommand || this.command,
+				users:			this.users
+			});
+			break;
+
+		case "toggle":
+			var i, u, item;
+			var count;
+
+			/* Count the number of users that are currently enabled */
+			for (count = 0, i = 0; u = this.users[i]; i++) {
+				if (u.enabled) {
+					count++;
+				}
+			}
+
+			for (i = 0; u = this.users[i]; i++) {
+				if (sender.userid != u.id) {
+					continue;
+				}
+
+				/* Toggle */
+				if (count <= 1 && u.enabled) {
+					/* At least one must always be enabled */
+					continue;
+				}
+
+				u.enabled = !u.enabled;
+				break;
+			}
+
+			/* Update styles */
+			for (i = 0; u = this.users[i]; i++) {
+				item = this.$['user' + i];
+
+				if (u.enabled) {
+					item.addClass('enabled');
+				} else {
+					item.removeClass('enabled');
+				}
+			}
+			break;
+	}
+
+	return(true);
+}
+
+});
+
+
